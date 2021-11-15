1bdcde505d485966fc512264975b9308fd721932
k41205 <giacomocavarretta@gmail.com>
2021-11-15 07:15:14 +0000

    first release 1.0.0

--- index.js
+++ index.js
@@ -4,6 +4,24 @@
 - reorganize code in modules
 - implement try-catch to manage errors and discard promise when rejected
 - make unavailable search till the autocomplete function is ready
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCaHjS973LOXtQK8nZL8uXok5bx4gZYeN4",
+  authDomain: "one-other-league.firebaseapp.com",
+  projectId: "one-other-league",
+  storageBucket: "one-other-league.appspot.com",
+  messagingSenderId: "929970509195",
+  appId: "1:929970509195:web:961c4b0f293a0f54908b8e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 */
 
 // FUNCTIONS
